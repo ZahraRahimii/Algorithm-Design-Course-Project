7e4da3ee82c1097aca024faabcc6e98c6cac1afd
unknown <zahra.rahimi.zrh@gmail.com>
2022-05-05 19:39:40 +0430

    Push Algorithm Design Project

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Eight_Minister eight_minister = new Eight_Minister();
+        eight_minister.main_func();
+    }
+}
+
