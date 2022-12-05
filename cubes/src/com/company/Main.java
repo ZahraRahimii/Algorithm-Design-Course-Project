65c345dab9aca6945cbb26233ce3142a0005d45f
ZahraRahimii <93929227+ZahraRahimii@users.noreply.github.com>
2022-12-05 14:17:55 +0330

    Update Main.java

--- cubes/src/com/company/Main.java
+++ cubes/src/com/company/Main.java
@@ -5,19 +5,7 @@ public class Main {
         Weighted_Colored_Cube colored_weighted_cube = new Weighted_Colored_Cube();
         colored_weighted_cube.main_func();
     }
-    private static void this_example(int n) {
-
-        if (n<=1)
-            return;
-
-        for (int i=0; i < n*n; i++){
-            this_example(n / 2);
-            this_example(n / 4);
-            this_example(n / 4);
-        }
-    }
-
-
+   
 }
 
 
